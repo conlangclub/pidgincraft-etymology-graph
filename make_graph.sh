f2d9013e4f19fe9832749a43a4454d53a2a13604
fechan <fred@fredchan.org>
2024-11-27 00:01:57 -0800

    fix maker script

--- make_graph.sh
+++ make_graph.sh
@@ -2,4 +2,4 @@
 
 git submodule update --remote --merge &&
 wget https://docs.google.com/spreadsheets/d/1_SCZqJMZ1UrwqbQ69Bxpf0JZVwz93x3tBZFf4DPrEDM/gviz/tq\?tqx\=out:csv\&sheet\=pidgincraft-vocab -O vocab.csv &&
-python3 ./vocab.csv
+python3 ./build_graph.py
