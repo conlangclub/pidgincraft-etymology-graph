77cd5e6298385988e64e724b8d75c73a1f9a0f38
fechan <fred@fredchan.org>
2024-12-10 21:56:17 -0800

    create build dir when making graph

--- make_graph.sh
+++ make_graph.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir build &&
 git submodule update --remote --merge &&
 wget https://docs.google.com/spreadsheets/d/1_SCZqJMZ1UrwqbQ69Bxpf0JZVwz93x3tBZFf4DPrEDM/gviz/tq\?tqx\=out:csv\&sheet\=pidgincraft-vocab -O vocab.csv &&
 python3 ./build_graph.py
